bf93b199c5b90d604cac814ffc061755dae646a3
David Luna Naranjo <davidluna.fn@gmail.com>
2024-09-15 08:35:10 -0500

    feat: add logger and config project

---
+++ {{ cookiecutter.project_slug }}/src/config/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+from functools import cached_property
+from google.cloud import secretmanager
+
+
+class Config(BaseSettings):
+
+    PROJECT_NAME: str = "{{ cookiecutter.project_slug }}"
+    PROJECT_NUMBER: str = ""
+    PROJECT_ID: str = ""
+    DATASET_ID: str = ""
+
+    @cached_property
+    def OPENAI_API_KEY(self):
+        return self.access_secret("OPENAI_API_KEY")
+
+    def access_secret(self, secret_name):
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/{self.PROJECT_NUMBER}/secrets/{secret_name}/versions/latest"
+        response = client.access_secret_version(request={"name": name})
+        return response.payload.data.decode("UTF-8")
+
+
+config = Config()
---
+++ {{ cookiecutter.project_slug }}/src/config/logger.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from .config import config
+
+class LogConfig(BaseModel):
+    """Logging configuration to be set for the server"""
+
+    LOGGER_NAME: str = "uvicorn"
+    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
+    LOG_LEVEL: str = "DEBUG"
+
+    # Logging config
+    version: int = 1
+    disable_existing_loggers: bool = False
+    formatters: dict = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers: dict = {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    }
+    loggers: dict = {
+        LOGGER_NAME: {
+          "handlers": ["default"],
+          "level": LOG_LEVEL,
+          "propagate": False},
+        config.PROJECT_NAME: {
+            "handlers": ["default"],
+            "level": LOG_LEVEL,
+            "propagate": False
+        }
+    }
+
+dictConfig(LogConfig().model_dump())
+logger = logging.getLogger(config.PROJECT_NAME)
+logger.info("Configuration finished")
